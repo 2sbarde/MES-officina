451904bfd4a52e767d8542189c8bbea7d3c7cdce
Davide <davide.reggio@gmail.com>
2026-03-22 17:30:43 +0100

    storico completo

---
+++ src/main/java/com/mes/mes_officina/OrdineRepository.java
@@ -0,0 +1,6 @@
+package com.mes.mes_officina;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrdineRepository extends JpaRepository<OrdineProduzione, Long> {
+}
