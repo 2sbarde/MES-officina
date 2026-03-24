5f78884a56d047d58fc41b63387e0050530d5a3d
Davide <davide.reggio@gmail.com>
2026-03-24 22:12:34 +0100

    update MES

--- src/main/java/com/mes/mes_officina/MesOfficinaApplication.java
+++ src/main/java/com/mes/mes_officina/MesOfficinaApplication.java
@@ -1,4 +1,4 @@
-package com.mes.mes_officina;
+package com.mes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class MesOfficinaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MesOfficinaApplication.class, args);
 	}
-
 }
